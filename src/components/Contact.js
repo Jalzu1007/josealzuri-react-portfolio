5a412276a833bfb398da77c412e99fa74812b856
Jose Alzuri <jalzu1007@gmail.com>
2023-09-07 17:34:12 -0400

    Added paragraph in contact file for the portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,9 @@ const Contact = () => {
     <section id="contact" className="py-5 text-center d-flex justify-content-center align-items-center">
     <div className="container section-content">
         <h2 className="text-center">Contact</h2>
+        <p>
+        I'm always eager to collaborate with like-minded individuals and organizations that share a passion for creating exceptional digital experiences. Whether you have a project in mind or just want to connect and discuss the latest trends in web development, I'd love to hear from you. Let's turn your ideas into reality and shape the future of the web together.
+        </p>
         <div className="row">
           <div className="col-md-6 mx-auto">
             <div className="card p-3">
