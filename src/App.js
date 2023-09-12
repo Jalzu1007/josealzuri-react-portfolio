e901025217834d03bc44b9a4f0858557a21211f8
Jose Alzuri <jalzu1007@gmail.com>
2023-09-12 11:55:47 -0400

    Updated package.json to fix errors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 import Header from './components/Header';
 import Home from './components/Home';
-import About from './components/about';
+import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
