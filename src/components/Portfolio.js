55c9e6a6a9ff576b67a43701b7126e53a8d5cb0e
Jose Alzuri <jalzu1007@gmail.com>
2023-10-09 18:31:39 -0400

    Updated projects file and images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,17 +4,17 @@ import Project from './Project';
 const projects = [
     {
       title: 'MyHealthSpace',
-      description: 'Full-Stack web application using MERN, GraphQL, and API.',
+      description: 'Full-Stack web application using MERN, GraphQL, and React.',
       imageSrc: 'my-health-space.jpg',
       deployedLink: 'https://my-health-space-e9d209a11adb.herokuapp.com/',
       githubLink: 'https://github.com/Jalzu1007/myhealthspace.git'
     },
     {
-      title: 'Tech Blog',
-      description: 'Full-Stack tech blog application.',
-      imageSrc: 'tech-blog-project.jpg',
-      deployedLink: 'https://tech-blog-jalzu1007-7d5a00d9e01b.herokuapp.com/',
-      githubLink: 'https://github.com/Jalzu1007/tech-blog.git'
+      title: 'ShelfSwap',
+      description: 'Full-Stack web application using MERN, GraphQL, React, and MaterialUI',
+      imageSrc: 'shelf-swap.jpg',
+      deployedLink: 'https://shelf-swap-425acc537cee.herokuapp.com/',
+      githubLink: 'https://github.com/Jalzu1007/ShelfSwap.git'
     },
     {
       title: 'BookMark',
@@ -31,18 +31,18 @@ const projects = [
       githubLink: 'https://github.com/Jalzu1007/the_recipe_bar.git'
     },
     {
+      title: 'Tech Blog',
+      description: 'Full-Stack tech blog application.',
+      imageSrc: 'tech-blog-project.jpg',
+      deployedLink: 'https://tech-blog-jalzu1007-7d5a00d9e01b.herokuapp.com/',
+      githubLink: 'https://github.com/Jalzu1007/tech-blog.git'
+    },
+    {
       title: 'Work Day Scheduler',
       description: 'Work Day Scheduler built using JavaScript, jQuery and DayJS.',
       imageSrc: 'work-day-scheduler.jpg',
       deployedLink: 'https://jalzu1007.github.io/work-day-scheduler/',
       githubLink: 'https://github.com/Jalzu1007/work-day-scheduler.git'
-    },
-    {
-      title: 'Password Generator',
-      description: 'This is a password generator built on JavaScript.',
-      imageSrc: 'password-generator.jpg',
-      deployedLink: 'https://jalzu1007.github.io/js-password-generator/',
-      githubLink: 'https://github.com/Jalzu1007/js-password-generator.git'
     },
   ];
   
